76c7d0d4ce2a88231fc1bc2dcbf9c12673244014
rmyers <rmyers@ncsu.edu>
2017-02-16 04:33:11 -0500

    Authentication starting to work

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :friends
+  has_many :accounts
+
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, :presence => true, :uniqueness => true, :length => { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }
+end
