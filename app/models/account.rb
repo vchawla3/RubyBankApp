077d396d4cb131af57a75605a9cb4b756156ed9d
rmyers <rmyers@ncsu.edu>
2017-02-18 15:46:35 -0500

    Accounts parameters

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,6 @@ class Account < ApplicationRecord
   belongs_to :user
   has_many :transactions
 
-  validates :acc_number, :presence => true
-  validates :balance, :presence => true
+  validates :acc_number, :presence => true, :length => {minimum: 9, maximum: 9}, :uniqueness => true
   validates :user_id, :presence => true
 end
