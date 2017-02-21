55dc2f893c18b4bf183b7af58749be1c446c12e1
vchawla3 <vchawla3@ncsu.edu>
2017-02-21 14:49:09 -0500

    updating validation for account balance

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,5 @@ class Account < ApplicationRecord
 
   validates :acc_number, :presence => true, :length => {minimum: 9, maximum: 9}, :uniqueness => true
   validates :user_id, :presence => true
+  validates :balance, numericality: { :greater_than_or_equal_to => 0 }
 end
